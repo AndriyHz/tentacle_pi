4b7cd9fc701cf0fbd0a2d1bbe12b5ca68e2875b4
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2015-09-05 00:04:32 +0200

    update comment header in all files

--- setup.py
+++ setup.py
@@ -1,5 +1,22 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+"""
+Tentacle Pi - A collection of drivers for TWI/SMBus devices.
+Copyright (c) 2015 Alexander Rüedlinger <a.rueedlinger@gmail.com>
+
+This file is part of Tentacle Pi:
+https://github.com/lexruee/tentacle_pi
+
+Tentacle Pi is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+Tentacle Pi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+"""
 
 from setuptools import setup, find_packages
 
--- tentacle_pi/twi/sensor/am2321.py
+++ tentacle_pi/twi/sensor/am2321.py
@@ -4,12 +4,15 @@
 Tentacle Pi - A collection of drivers for TWI/SMBus devices.
 Copyright (c) 2015 Alexander Rüedlinger <a.rueedlinger@gmail.com>
 
-This program is free software; you can redistribute it and/or modify
+This file is part of Tentacle Pi:
+https://github.com/lexruee/tentacle_pi
+
+Tentacle Pi is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
 (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
+Tentacle Pi is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
