c6ee4cb7672481fe4385413a80908dd9a1052a50
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2015-09-04 19:08:54 +0200

    add driver for bmp85

---
+++ tentacle_pi/twi/sensor/bmp85.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+"""
+Tentacle Pi - A collection of drivers for TWI/SMBus devices.
+Copyright (c) 2015 Alexander Rüedlinger <a.rueedlinger@gmail.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+"""
+
+__author__ = 'Alexander Rüedlinger'
+__all__ = ['BMP85']
+
+from tentacle_pi.twi.sensor.bmp180 import BMP180
+
+
+class BMP85(BMP180):
+
+    def __init__(self, addr=0x77, bus=1):
+        super(BMP85, self).__init__(addr, bus)
