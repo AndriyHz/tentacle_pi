a67ba3bdc4f1d5ece116a67d804bbfffa81cd3a2
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2015-09-07 20:34:21 +0200

    fix HDC1008 example

--- examples/hdc1008_example.py
+++ examples/hdc1008_example.py
@@ -2,18 +2,18 @@
 
 """
 @author     Alexander Rüedlinger <a.rueedlinger@gmail.com>
-@date       21.08.2015
+@date       7.09.2015
 
 """
 
 import time
 
-from tentacle_pi.twi.sensor.am2315 import AM2315
+from tentacle_pi.twi.sensor.hdc1008 import HDC1008
 
 
-with AM2315() as am:
+with HDC1008() as hdc:
     for i in range(0, 5):
-        measurement = am.measure()
+        measurement = hdc.measure()
         for key, val in measurement.items():
             print("%s: %s" % (key, val))
         print("")
